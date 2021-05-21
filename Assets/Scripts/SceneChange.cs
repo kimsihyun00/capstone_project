b65ca0c9265c70e01224219561279836fa04dfe0
kimsihyun00 <67780017+kimsihyun00@users.noreply.github.com>
2021-05-21 15:37:56 +0900

    drawing line test 3d to 2d

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("LineScene");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+    private void SavePrefab()
+    {
+        string fileName = "Line";
+        string path = "Asset/Resources/" + fileName + ".prefab";
+        Object obj = UnityEditor.AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(path);
+        GameObject temp = Instantiate(obj) as GameObject;
+
+        bool isSuccess = false;
+        UnityEditor.PrefabUtility.SaveAsPrefabAsset(temp, path, out isSuccess);
+        UnityEditor.AssetDatabase.Refresh();
+        Debug.Log(isSuccess);
+    }
+}
